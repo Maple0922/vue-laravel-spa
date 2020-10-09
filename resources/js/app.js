465c3da067ded3d3081d06d5812a7e9ca184a199
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-10-10 06:41:12 +0900

    add header component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import HeaderComponent from "./components/HeaderComponent";
 
 require('./bootstrap');
 
@@ -19,8 +20,8 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('header-component', HeaderComponent);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
