7882505ea9d32c0ff69b4fe1f102b219f8ad51a4
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-10-10 22:58:45 +0900

    add store and update method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
@@ -13,6 +14,17 @@ class TaskController extends Controller
 
     public function show(Task $task)
     {
+        return $task;
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
         return $task;
     }
 }
