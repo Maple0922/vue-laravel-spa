4296b860dd4e37a6a3fdde5fe0437abbdaf54eed
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-10-10 22:31:33 +0900

    update edit page default value

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -9,15 +9,15 @@
                     </div>
                     <div class="form-group row">
                         <label for="title" class="col-sm-3 col-form-label">Title</label>
-                        <input type="text" class="col-sm-9 form-control" id="title" :value="">
+                        <input type="text" class="col-sm-9 form-control" id="title">
                     </div>
                     <div class="form-group row">
                         <label for="content" class="col-sm-3 col-form-label">Content</label>
-                        <input type="text" class="col-sm-9 form-control" id="content" :value="">
+                        <input type="text" class="col-sm-9 form-control" id="content">
                     </div>
                     <div class="form-group row">
                         <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
-                        <input type="text" class="col-sm-9 form-control" id="person-in-charge" :value="">
+                        <input type="text" class="col-sm-9 form-control" id="person-in-charge">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
