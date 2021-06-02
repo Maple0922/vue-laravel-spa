1620796a9f369f1b6a069f3bd883443c130a22a4
Futo Nakajima <maple0922wh-142857@i.softbank.jp>
2021-06-02 21:32:34 +0900

    add: 様々な処理で注入されたモデルを使う

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class TaskController extends Controller
 {
@@ -15,31 +14,49 @@ class TaskController extends Controller
 
     public function index()
     {
+        // where系 有効
+        return $this->task->whereNotNull('id')->get();
+        return $this->task->where('id', 4)->get();
+
+        // all 有効
         return $this->task->all();
     }
 
     public function store(Request $request)
     {
-        // return Task::create($request->all());
+        // create 有効
         return $this->task->create($request->all());
     }
 
     public function show(Task $task)
     {
+        // メソッドインジェクション 有効
         return $task;
+
+        // find 有効
+        return $this->task->find($task->id);
     }
 
     public function update(Request $request, Task $task)
     {
+        // メソッドインジェクション 有効
         $task->update($request->all());
+        return $task;
 
+        // fill 有効
+        $task = $this->task->find($request->id);
+        $task->fill($request->all())->save();
         return $task;
     }
 
     public function destroy(Task $task)
     {
+        // メソッドインジェクション 有効
         $task->delete();
+        return $task;
 
+        // delete 有効
+        $this->task->find($task->id)->delete();
         return $task;
     }
 }
